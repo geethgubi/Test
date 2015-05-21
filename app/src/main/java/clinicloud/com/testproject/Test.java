899d8736e9e9b4baba1d51747ae3fa39179b2a69
Geetha Gubendran <geetha.gubendran@clinicloud.com>
2015-05-21 12:41:42 +1000

    commiting the chages of Test.java

--- app/src/main/java/clinicloud/com/testproject/Test.java
+++ app/src/main/java/clinicloud/com/testproject/Test.java
@@ -4,4 +4,7 @@ package clinicloud.com.testproject;
  * Created by geetha on 21/05/15.
  */
 public class Test {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+    }
 }
