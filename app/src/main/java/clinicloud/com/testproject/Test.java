34107e94b87e211ccdb6813c14056b50e9b4e567
Geetha Gubendran <geetha.gubendran@clinicloud.com>
2015-05-21 12:44:12 +1000

    commiting the chages of Test.java change1

--- app/src/main/java/clinicloud/com/testproject/Test.java
+++ app/src/main/java/clinicloud/com/testproject/Test.java
@@ -5,6 +5,6 @@ package clinicloud.com.testproject;
  */
 public class Test {
     public static void main(String args[]) {
-        System.out.println("Hello");
+        System.out.println("Hi");
     }
 }
